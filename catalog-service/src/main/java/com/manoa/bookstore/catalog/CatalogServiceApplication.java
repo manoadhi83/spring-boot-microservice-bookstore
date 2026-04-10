af040238c6c6f8c935b5a5c280244f0ad347c2a1
manoa <mano.adhi83@gmail.com>
2026-04-10 12:24:23 -0700

    Add catalog-service

---
+++ catalog-service/src/main/java/com/manoa/bookstore/catalog/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manoa.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+        System.out.println("Workslow testing");
+    }
+}
