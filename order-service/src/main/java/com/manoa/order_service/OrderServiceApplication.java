ff9a6da2b713b4a2fffb0340f4474e4b785d3c66
manoa <mano.adhi83@gmail.com>
2026-04-18 17:18:35 -0700

    Testing git workflow for order service

--- order-service/src/main/java/com/manoa/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/manoa/order_service/OrderServiceApplication.java
@@ -8,5 +8,6 @@ public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+        System.out.print("Testing workflow");
     }
 }
